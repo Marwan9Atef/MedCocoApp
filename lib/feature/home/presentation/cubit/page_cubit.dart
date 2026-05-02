3ea2efdbb87a75990f108f041f744b7bc741b21c
Marwan Atef <marwanatef.mahmoud@gmail.com>
2026-05-03 01:19:11 +0300

    fix:solve history rebuild bug when user back from search

--- lib/feature/home/presentation/cubit/page_cubit.dart
+++ lib/feature/home/presentation/cubit/page_cubit.dart
@@ -4,6 +4,15 @@ class PageCubit extends Cubit<int> {
   PageCubit() : super(0);
   void setValue(int newValue) => emit(newValue);
 
+  int _historyKey = 0;
+  int get historyKey => _historyKey;
 
+  void destroyHistory() {
+    _historyKey++;
+    emit(state);
+  }
 
+  void resetHistoryKey() {
+    _historyKey = 0;
+  }
 }
